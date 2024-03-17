d26fb3717b799ee698e5c4ddad2c6f78d666c887
Cortney M <cortneym163@gmail.com>
2024-03-17 13:31:11 -0400

    commit on main

---
+++ MapleQuestion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class MapleQuestion {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What class is your favorite in MapleStory? ");
+        String favorite = scan.nextLine();
+        System.out.println(" My favorite class is " + favorite);
+    }
+}
