d0fe1d5edd31ee92c0d4f418496ef96a4887e1fd
Cortney M <cortneym163@gmail.com>
2024-03-17 13:37:13 -0400

    commit on main

--- MapleQuestion.java
+++ MapleQuestion.java
@@ -5,6 +5,9 @@ public class MapleQuestion {
         Scanner scan = new Scanner(System.in);
         System.out.println("What class is your favorite in MapleStory? ");
         String favorite = scan.nextLine();
+        System.out.println("What level is your main? ");
+        int level = scan.nextInt();
         System.out.println(" My favorite class is " + favorite);
+        System.out.println("My main is currently level " + level);
     }
 }
