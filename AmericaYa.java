d565c58b436a16cec43d397c1b4b8bf20e35b898
Cortney M <cortneym163@gmail.com>
2024-04-07 09:55:00 -0400

    commit on main

---
+++ AmericaYa.java
@@ -0,0 +1,9 @@
+public class AmericaYa{
+    
+    
+    public static void main (String[]args){
+System.out.println("America yaaaa!");
+System.out.println(" Americaaaaa ");
+System.out.println(" Hallo! Hallo!! Halloooo :3");
+}
+}
